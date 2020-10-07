923b1f9f7692dc17633afc27b46b9d9264869e27
Leader <leader228228@gmail.com>
2020-10-07 03:13:41 +0300

    [+] CLICommandExecutionResult javadoc added

--- src/main/java/ua/edu/sumdu/elit/in71/tss2020t3/pulsar/CLI/CLICommandExecutionResult.java
+++ src/main/java/ua/edu/sumdu/elit/in71/tss2020t3/pulsar/CLI/CLICommandExecutionResult.java
@@ -1,8 +1,22 @@
 package ua.edu.sumdu.elit.in71.tss2020t3.pulsar.CLI;
 
+import picocli.CommandLine;
+
 /**
  * Interface-container for a {@link ua.edu.sumdu.elit.in71.tss2020t3.pulsar.CLI.commands.CLICommand} execution result
  * */
 public interface CLICommandExecutionResult<T> {
+
+	/**
+	 * Constant that indicates that something went wrong during
+	 * {@link ua.edu.sumdu.elit.in71.tss2020t3.pulsar.CLI.commands.CLICommand} execution
+	 *
+	 * @see            CommandLine#execute(String...)
+	 * */
+	int UNKNOWN_ERROR_EXECUTION_RESULT_CODE = -1;
+
+	/**
+	 * @return			a command execution result
+	 * */
 	T getResult();
 }

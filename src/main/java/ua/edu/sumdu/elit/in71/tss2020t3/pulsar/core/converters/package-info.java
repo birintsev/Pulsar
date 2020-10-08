d28358837280c168abc0afa19d2f4c5291797cd3
Leader <leader228228@gmail.com>
2020-10-08 23:43:28 +0300

    [+] package-info.java for ua.edu.sumdu.elit.in71.tss2020t3.pulsar.core.converters added

---
+++ src/main/java/ua/edu/sumdu/elit/in71/tss2020t3/pulsar/core/converters/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * This is a package of custom converters
+ *
+ * <p>
+ *     Below is a classes naming policy for this package. It is preferable to keep it, but is not mandatory.
+ * </p>
+ * <p>
+ *     A class name should consist of ${<b>source class name</b>}<b>2</b>${<b>result class name</b>}<b>Converter</b>.
+ * </p>
+ *
+ * @see            com.fasterxml.jackson.databind.util.StdConverter
+ * @see            com.fasterxml.jackson.databind.util.Converter
+ * */
+package ua.edu.sumdu.elit.in71.tss2020t3.pulsar.core.converters;

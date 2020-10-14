fc9b041568af42755bf0460b3580c44307166766
Leader <leader228228@gmail.com>
2020-10-14 11:36:24 +0300

    [~] User user id encapsulated; age, phone number properties added [~] entities-mapping.hbm.xml changes according to User.java

--- src/main/java/ua/edu/sumdu/elit/in71/tss2020t3/pulsar/core/entities/User.java
+++ src/main/java/ua/edu/sumdu/elit/in71/tss2020t3/pulsar/core/entities/User.java
@@ -1,5 +1,6 @@
 package ua.edu.sumdu.elit.in71.tss2020t3.pulsar.core.entities;
 
+import java.util.Objects;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,9 +15,84 @@ import java.io.Serializable;
 @AllArgsConstructor
 public class User implements Serializable {
 
+    private UserID id;
+
     private String firstName;
 
     private String lastName;
 
-    private String email;
+    private int age;
+
+    private String phoneNumber;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        User user = (User) o;
+
+        if (age != user.age) {
+            return false;
+        }
+        if (!id.equals(user.id)) {
+            return false;
+        }
+        if (!firstName.equals(user.firstName)) {
+            return false;
+        }
+        if (!lastName.equals(user.lastName)) {
+            return false;
+        }
+        return Objects.equals(phoneNumber, user.phoneNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + firstName.hashCode();
+        result = 31 * result + lastName.hashCode();
+        result = 31 * result + age;
+        result = 31 * result
+            + (phoneNumber != null ? phoneNumber.hashCode() : 0);
+        return result;
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class UserID implements Serializable {
+
+        private String email;
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+
+            UserID userID = (UserID) o;
+
+            return email.equals(userID.email);
+        }
+
+        @Override
+        public int hashCode() {
+            return email.hashCode();
+        }
+
+        @Override
+        public String toString() {
+            return "UserID{" +
+                "email='" + email + '\'' +
+                '}';
+        }
+    }
 }

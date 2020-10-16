b3eedb3f2c40b01e6fdfab43e5d9bdfd9e0be5f2
Leader <leader228228@gmail.com>
2020-10-16 15:09:37 +0300

    [+] ua.edu.sumdu.elit.in71.tss2020t3.pulsar.protocolhandlers package added (support of custom URL protocols)

---
+++ src/main/java/ua/edu/sumdu/elit/in71/tss2020t3/pulsar/protocolhandlers/package-info.java
@@ -0,0 +1,15 @@
+/**
+ * This is a base package of custom {@link java.net.URLStreamHandler}
+ * implementations which provide support for different of {@link java.net.URI}
+ * protocols.
+ * <p>
+ * This package may be set as a base package of custom
+ * {@link java.net.URLStreamHandler}s (i.e. set as the
+ * {@code java.protocol.handler.pkgs} {@link java.lang.System} property value)
+ * or used in a custom {@link java.net.URLStreamHandlerFactory}.
+ *
+ * @see java.net.URI
+ * @see java.net.URL
+ * @see java.net.URLStreamHandlerFactory
+ * */
+package ua.edu.sumdu.elit.in71.tss2020t3.pulsar.protocolhandlers;

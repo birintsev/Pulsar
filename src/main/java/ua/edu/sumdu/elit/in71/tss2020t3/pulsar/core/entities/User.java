9406cf433ff4977f6c1800a24db4a578b71ca89d
Leader <leader228228@gmail.com>
2020-10-25 18:43:46 +0200

    [~] User#userStatuses field added

--- src/main/java/ua/edu/sumdu/elit/in71/tss2020t3/pulsar/core/entities/User.java
+++ src/main/java/ua/edu/sumdu/elit/in71/tss2020t3/pulsar/core/entities/User.java
@@ -1,7 +1,7 @@
 package ua.edu.sumdu.elit.in71.tss2020t3.pulsar.core.entities;
 
 import java.io.Serializable;
-import java.util.Objects;
+import java.util.HashSet;
 import java.util.Set;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -33,65 +33,7 @@ public class User implements Serializable {
 
     private String password;
 
-    private Set<UserStatus> userStatuses;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-
-        User user = (User) o;
-
-        if (age != user.age) {
-            return false;
-        }
-        if (!id.equals(user.id)) {
-            return false;
-        }
-        if (!username.equals(user.username)) {
-            return false;
-        }
-        if (!Objects.equals(firstName, user.firstName)) {
-            return false;
-        }
-        if (!Objects.equals(lastName, user.lastName)) {
-            return false;
-        }
-        if (!Objects.equals(phoneNumber, user.phoneNumber)) {
-            return false;
-        }
-        return password.equals(user.password);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id.hashCode();
-        result = 31 * result + username.hashCode();
-        result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
-        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
-        result = 31 * result + age;
-        result =
-            31 * result + (phoneNumber != null ? phoneNumber.hashCode() : 0);
-        result = 31 * result + password.hashCode();
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "User{"
-            + "id=" + id
-            + ", username='" + username + '\''
-            + ", firstName='" + firstName + '\''
-            + ", lastName='" + lastName + '\''
-            + ", age=" + age
-            + ", phoneNumber='" + phoneNumber + '\''
-            + ", password='" + "********" + '\''
-            + '}';
-    }
+    private Set<UserStatus> userStatuses = new HashSet<>();
 
     @Data
     @NoArgsConstructor
